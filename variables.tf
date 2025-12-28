8901e626488ade6d67c1e3162297a85b043f077c
Sandeep <54panditsandeep@gmail.com>
2025-12-28 13:32:57 +0530

    added required variables for eks cluster

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "cluster_name" {
+  default = "sandeep-cluster-1"
+}
+
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "node_instance_type" {
+  default = "t3.medium"
+}
+
+variable "desired_size" {
+  default = 2
+}
